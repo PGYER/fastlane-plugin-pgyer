9411d781a0974bb6667130f52079978789071f64
rexshi <rexshi@pgyer.com>
2017-01-21 17:13:28 +0800

    pgyer service

--- lib/fastlane/plugin/pgyer/actions/pgyer_action.rb
+++ lib/fastlane/plugin/pgyer/actions/pgyer_action.rb
@@ -1,8 +1,56 @@
+require 'faraday'
+require 'faraday_middleware'
+
 module Fastlane
   module Actions
     class PgyerAction < Action
+
       def self.run(params)
         UI.message("The pgyer plugin is working!")
+
+        api_host = "http://qiniu-storage.pgyer.com/apiv1/app/upload"
+        api_key = params[:api_key]
+        user_key = params[:user_key]
+
+        build_file = [
+            params[:ipa],
+            params[:apk]
+        ].detect { |e| !e.to_s.empty? }
+
+        if build_file.nil?
+            UI.user_error!("You have to provide a build file")
+        end
+
+        UI.message "build_file: #{build_file}"
+
+        # start upload
+        conn_options = {
+          request: {
+            timeout:       1000,
+            open_timeout:  300
+          }
+        }
+
+        pgyer_client = Faraday.new(nil, conn_options) do |c|
+            c.request :multipart
+            c.request :url_encoded
+            c.response :json, content_type: /\bjson$/
+            c.adapter :net_http
+        end
+
+        params = {
+            '_api_key' => api_key,
+            'uKey' => user_key,
+            'file' => Faraday::UploadIO.new(build_file, 'application/octet-stream')
+        }
+
+        UI.message "Start upload #{build_file} to pgyer..."
+
+        response = pgyer_client.post api_host, params
+        info = response.body
+
+        UI.success "Upload success. Visit this URL to see: https://www.pgyer.com/#{info['data']['appShortcutUrl']}"
+
       end
 
       def self.description
@@ -24,11 +72,40 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "PGYER_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :api_key,
+                                  env_name: "PGYER_API_KEY",
+                               description: "api_key in your pgyer account",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :user_key,
+                                  env_name: "PGYER_USER_KEY",
+                               description: "user_key in your pgyer account",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :apk,
+                                       env_name: "FL_HOCKEY_APK",
+                                       description: "Path to your APK file",
+                                       default_value: Actions.lane_context[SharedValues::GRADLE_APK_OUTPUT_PATH],
+                                       optional: true,
+                                       verify_block: proc do |value|
+                                         UI.user_error!("Couldn't find apk file at path '#{value}'") unless File.exist?(value)
+                                       end,
+                                       conflicting_options: [:ipa],
+                                       conflict_block: proc do |value|
+                                         UI.user_error!("You can't use 'apk' and '#{value.key}' options in one run")
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :ipa,
+                                       env_name: "PGYER_IPA",
+                                       description: "Path to your IPA file. Optional if you use the _gym_ or _xcodebuild_ action. For Mac zip the .app. For Android provide path to .apk file",
+                                       default_value: Actions.lane_context[SharedValues::IPA_OUTPUT_PATH],
+                                       optional: true,
+                                       verify_block: proc do |value|
+                                         UI.user_error!("Couldn't find ipa file at path '#{value}'") unless File.exist?(value)
+                                       end,
+                                       conflicting_options: [:apk],
+                                       conflict_block: proc do |value|
+                                         UI.user_error!("You can't use 'ipa' and '#{value.key}' options in one run")
+                                       end),
         ]
       end
 
@@ -36,7 +113,7 @@ module Fastlane
         # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
         # See: https://github.com/fastlane/fastlane/blob/master/fastlane/docs/Platforms.md
         #
-        # [:ios, :mac, :android].include?(platform)
+        [:ios, :mac, :android].include?(platform)
         true
       end
     end
