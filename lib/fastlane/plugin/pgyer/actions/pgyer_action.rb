78a2bd0c3f268719217990a827a823408ff537b4
DevZhang <me@devorz.com>
2025-09-12 16:10:42 +0800

    fix：Multiple entries for configuration key 'hap' found!

--- lib/fastlane/plugin/pgyer/actions/pgyer_action.rb
+++ lib/fastlane/plugin/pgyer/actions/pgyer_action.rb
@@ -167,17 +167,6 @@ module Fastlane
                                        conflict_block: proc do |value|
                                          UI.user_error!("You can't use 'apk' and '#{value.key}' options in one run")
                                        end),
-          FastlaneCore::ConfigItem.new(key: :hap,
-                                       env_name: "PGYER_HAP",
-                                       description: "Path to your HAP file",
-                                       optional: true,
-                                       verify_block: proc do |value|
-                                         UI.user_error!("Couldn't find hap file at path '#{value}'") unless File.exist?(value)
-                                       end,
-                                       conflicting_options: [:ipa, :apk],
-                                       conflict_block: proc do |value|
-                                         UI.user_error!("You can't use 'hap' and '#{value.key}' options in one run")
-                                       end),
           FastlaneCore::ConfigItem.new(key: :ipa,
                                        env_name: "PGYER_IPA",
                                        description: "Path to your IPA file. Optional if you use the _gym_ or _xcodebuild_ action. For Mac zip the .app. For Android provide path to .apk file",
